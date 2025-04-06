31c06551825f5ecb8b66642a5bb06fab14677ae0
liao749 <liao.749@osu.edu>
2025-04-05 20:36:46 -0400

    edit models to add constraints

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,6 @@
 class Participant < ApplicationRecord
   belongs_to :tripName
   belongs_to :user
+  has_and_belongs_to_many :expenses
+  has_many :expenses_paid, class_name :"Expense", foreign_key :"payer_id"
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,5 @@
 class Trip < ApplicationRecord
   belongs_to :user
+  has_many :participants
+  has_many :expenses
 end
