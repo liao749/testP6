320e1cedd1a6e2f47884defa4217ba959095c6f9
liao749 <liao.749@osu.edu>
2025-04-05 21:09:41 -0400

    update expense.rb

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ApplicationRecord
-  belongs_to :tripName
-  belongs_to :payer
+  belongs_to :trip
+  belongs_to :category
+  belongs_to :payer, class_name: 'Participant'
+  has_and_belongs_to_many :participants
 end
