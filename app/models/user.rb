093f16400195c467f167a52e2c00de23920f8786
liao749 <liao.749@osu.edu>
2025-04-05 23:03:26 -0400

    re create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,2 @@
 class User < ApplicationRecord
-  has_many :trips
-  has_many :participants
-  has_many :expenses, foreign_key: "payer_id"
-  validates :name, presence:true, length:{ minimum: 1,maximum: 20}
-   validates :email, presence:true, length:{ maximum: 100},
-   uniqueness:true,format:{with:
-   /\A[A-Za-z]+\.\d+@osu\.edu\Z/i }
 end
