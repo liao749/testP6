88d60dbb32cf79a931655f85d90a1529431e42be
liao749 <liao.749@osu.edu>
2025-04-05 22:32:12 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :trips
   has_many :participants
   has_many :expenses, foreign_key: "payer_id"
+  validates :name, presence:true, length:{ minimum: 1,maximum: 20}
+   validates :email, presence:true, length:{ maximum: 100},
+   uniqueness:true,format:{with:
+   /\A[A-Za-z]+\.\d+@osu\.edu\Z/i }
 end
